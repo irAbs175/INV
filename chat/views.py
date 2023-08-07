7f5b20e2703bcc0800304db6a76402d3c5b21b71
Abbas Damerchilo <damerchiloa@gmail.com>
2023-08-07 09:25:34 +0000

    update

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from account.models import user_accounts
 
 
+@login_required
 def pvmessage(request, room):
-    return render(request, "chat/public.html", {"room_name": "TST"})
+    account = user_accounts.objects.filter(last_name = room)
+    if account.exists():
+        return render(request, "chat/private.html", {"room_name": room})
+    else:
+        return render(request, "dashboard/dashboard.html")
